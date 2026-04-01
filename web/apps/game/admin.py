a978d4ef734927f903d4710463c676fdf0beacc3
124476 <124476@bk.ru>
2026-04-01 19:21:29 +0300

    Uplode: admin

---
+++ web/apps/game/admin.py
@@ -0,0 +1,56 @@
+__all__ = ()
+
+from apps.game.models import Game, GamePlayer, GameHolding, GameTransaction
+
+from django.contrib import admin
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "creator",
+        "status",
+        "created_at",
+    )
+    list_filter = ("status",)
+    search_fields = (
+        "name",
+        "creator__username",
+    )
+
+
+@admin.register(GamePlayer)
+class GamePlayerAdmin(admin.ModelAdmin):
+    list_display = (
+        "player_name",
+        "game",
+        "user",
+        "cash",
+        "total_value",
+    )
+    list_filter = ("game",)
+    search_fields = (
+        "player_name",
+        "user__username",
+    )
+
+
+@admin.register(GameHolding)
+class GameHoldingAdmin(admin.ModelAdmin):
+    list_display = ("player", "stock", "quantity", "average_price")
+    search_fields = ("player__player_name", "stock__ticker")
+
+
+@admin.register(GameTransaction)
+class GameTransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "player",
+        "stock",
+        "type",
+        "quantity",
+        "price",
+        "created_at",
+    )
+    list_filter = ("type",)
+    search_fields = ("player__player_name", "stock__ticker")
---
+++ web/apps/trading/admin.py
@@ -0,0 +1,60 @@
+__all__ = ()
+
+from apps.trading.models import (
+    PersonalPortfolio,
+    PersonalHolding,
+    PersonalTransaction,
+)
+
+from django.contrib import admin
+
+
+@admin.register(PersonalPortfolio)
+class PersonalPortfolioAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "cash",
+        "total_value",
+        "updated_at",
+    )
+    search_fields = ("user__username",)
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
+
+
+@admin.register(PersonalHolding)
+class PersonalHoldingAdmin(admin.ModelAdmin):
+    list_display = (
+        "portfolio",
+        "stock",
+        "quantity",
+        "average_price",
+    )
+    search_fields = (
+        "portfolio__user__username",
+        "stock__ticker",
+    )
+
+
+@admin.register(PersonalTransaction)
+class PersonalTransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "portfolio",
+        "stock",
+        "type",
+        "quantity",
+        "price",
+        "total",
+        "created_at",
+    )
+    list_filter = (
+        "type",
+        "created_at",
+    )
+    search_fields = (
+        "portfolio__user__username",
+        "stock__ticker",
+    )
+    readonly_fields = ("created_at",)
